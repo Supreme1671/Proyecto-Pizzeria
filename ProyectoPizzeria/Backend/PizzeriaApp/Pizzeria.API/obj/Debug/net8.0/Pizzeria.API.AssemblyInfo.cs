f3d2b4531568029efb5c4ccc9620a9a5af4761df
Enzo Adriel Casimiro <enzocasimiro565@gmail.com>
2026-09-09 17:43:21 -0300

    Fabrizio lo hizo

--- ProyectoPizzeria/Backend/PizzeriaApp/Pizzeria.API/obj/Debug/net8.0/Pizzeria.API.AssemblyInfo.cs
+++ ProyectoPizzeria/Backend/PizzeriaApp/Pizzeria.API/obj/Debug/net8.0/Pizzeria.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pizzeria.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1616eb2edcfdc36900ca55fa88af79f35f7e22eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0be298dfb7ce6854de84d0c0749415ba7a00a3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pizzeria.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pizzeria.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
